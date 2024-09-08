6e11deb2e91249345b02b11db310fee9e941af5a
Vedant Bachhav <vedantbachhav108@gmail.com>
2024-09-08 16:26:58 +0530

    Basics of Array

---
+++ MultiDimensionArray.java
@@ -0,0 +1,25 @@
+package COM.DSA_Array;
+
+public class MultiDimensionArray {
+    public static void main(String[] args) {
+
+        System.out.println("Printing two Dimension Array.");
+        /*
+        It looks like matrix.
+        1 2 3
+        4 5 6
+        7 8 9
+        Syntax : int [] [] arr = new int[rows*] [col]; rows are mandatory.
+         */
+        // Method 1
+        int [] [] arr = new int[3][];
+        // Method 2 direct method.
+        int [] [] arrd={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+
+    }
+}
